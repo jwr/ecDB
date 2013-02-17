69be18318751e73536ab1732e9774e076312e057
Emil Rowland <rowland.emil@gmail.com>
2013-02-17 17:07:14 +0100

    Update include/autocomplete/autocomplete_name.php

--- include/autocomplete/autocomplete_name.php
+++ include/autocomplete/autocomplete_name.php
@@ -1,9 +1,19 @@
 <?php
 require_once "../mysql_connect.php";
+include ('../login/auth.php');
 $q = strtolower($_GET["q"]);
 if (!$q) return;
 
-$sql = "select DISTINCT name as name from data where name LIKE '%$q%' ORDER by name ASC";
+$owner	=	$_SESSION['SESS_MEMBER_ID'];
+			
+$user_setting_auto_complete = mysql_query("SELECT auto_complete FROM members WHERE member_id = ".$owner."");
+
+if(mysql_fetch_array($user_setting_auto_complete)['auto_complete'] == "user"){
+	$sql = "select DISTINCT name as name from data where name LIKE '%$q%' AND owner = ".$owner." ORDER by name ASC";
+}
+else{
+	$sql = "select DISTINCT name as name from data where name LIKE '%$q%' ORDER by name ASC";
+}
 $rsd = mysql_query($sql);
 while($rs = mysql_fetch_array($rsd)) {
 	$cname = $rs['name'];
