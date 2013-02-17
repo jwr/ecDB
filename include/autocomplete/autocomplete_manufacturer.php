232a541104581dd47779e30c2b9973b5498f6964
Emil Rowland <rowland.emil@gmail.com>
2013-02-17 17:06:41 +0100

    Update include/autocomplete/autocomplete_manufacturer.php

--- include/autocomplete/autocomplete_manufacturer.php
+++ include/autocomplete/autocomplete_manufacturer.php
@@ -1,9 +1,20 @@
 <?php
 require_once "../mysql_connect.php";
+include ('../login/auth.php');
 $q = strtolower($_GET["q"]);
 if (!$q) return;
 
-$sql = "select DISTINCT manufacturer as manufacturer from data where manufacturer LIKE '%$q%' ORDER by name ASC";
+$owner	=	$_SESSION['SESS_MEMBER_ID'];
+			
+$user_setting_auto_complete = mysql_query("SELECT auto_complete FROM members WHERE member_id = ".$owner."");
+
+if(mysql_fetch_array($user_setting_auto_complete)['auto_complete'] == "user"){
+	$sql = "select DISTINCT manufacturer as manufacturer from data where manufacturer LIKE '%$q%' AND owner = ".$owner." ORDER by name ASC";
+}
+else{
+	$sql = "select DISTINCT manufacturer as manufacturer from data where manufacturer LIKE '%$q%' ORDER by name ASC";
+}
+
 $rsd = mysql_query($sql);
 while($rs = mysql_fetch_array($rsd)) {
 	$manufacturer = $rs['manufacturer'];
