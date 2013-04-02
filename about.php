666726ce76f76502119ae3bafbdcd4d1728ab25d
Nils Fredriksson <hello@electricman.se>
2013-04-02 20:30:42 +0200

    Menu update on index-pages.

--- about.php
+++ about.php
@@ -26,10 +26,10 @@
 			<!-- Main menu -->
 			<div id="menu">
 				<ul>
-					<li><a href=".">Login</a></li>
-					<li><a href="register.php">Register</a></li>
-					<li><a href="about.php" class="selected">About</a></li>
-					<li><a href="/blog">Blog</a></li>
+					<li><a href="."><span class="icon medium key"></span> Login</a></li>
+					<li><a href="register.php"><span class="icon medium user"></span> Register</a></li>
+					<li><a href="about.php" class="selected"><span class="icon medium document"></span> About</a></li>
+					<li><a href="/blog"><span class="icon medium docLinesStright"></span> Blog</a></li>
 				</ul>
 			</div>
 			<!-- END -->
