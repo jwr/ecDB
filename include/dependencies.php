abe570549250ec6c6305b0e44a2137ed705dd367
Peeter Normak <peeter.normak@eesti.ee>
2017-05-29 20:03:52 +0300

    added smarty support

--- include/dependencies.php
+++ include/dependencies.php
@@ -3,3 +3,11 @@
 
 $container = $app->getContainer();
 
+$container['view'] = function ($container) use ($ECDB_VERSION) {
+    $smarty = new Smarty();
+    $smarty->setTemplateDir(__DIR__ . '/../views');
+    $smarty->assign('ECDB_VERSION', $ECDB_VERSION);
+    $smarty->error_reporting = E_ALL & ~E_NOTICE;
+
+    return $smarty;
+};
