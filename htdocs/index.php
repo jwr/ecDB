3d28c9c8a0b952a453c865a5da846aedc5b331f9
Peeter Normak <peeter.normak@eesti.ee>
2017-06-22 18:18:34 +0300

    support php built-in server

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * In case running PHP built-in web server, images/js/css requests will not be handled by this file, but server directly.
+ * In case running with Apache web server, this is handled with .htaccess.
+ */
+if (php_sapi_name() == 'cli-server' && preg_match('/\.(?:png|jpg|jpeg|gif|css|js|ico)$/', $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
 require_once __DIR__ . '/../setup.php';
 
 $app = new \Slim\App(array(
